834fc5fc618872dfe4dc7c027c470012998cc548
André Di Paulo <andredipaulo@gmail.com>
2023-11-14 14:33:37 -0300

    ajuste api

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost/api"
-    // baseURL: "http://192.168.100.24/api"
+    // baseURL: "http://localhost/api"
+    baseURL: "http://192.168.100.2/api"
 })
