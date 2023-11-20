7798d7bb5eb212473c3a21e93b19d59083d12dbf
André Di Paulo <andredipaulo@gmail.com>
2023-11-20 15:14:13 -0300

    Ajustes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,11 @@ export default function Document() {
         content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
       />
       
-      <meta name="application-name" content="PWA App" />
+      <meta name="application-name" content="Factory App" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
+      <meta name="apple-mobile-web-app-title" content="Factory App" />
+      <meta name="description" content="Factory controle de empréstimos" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -33,18 +33,18 @@ export default function Document() {
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
-      <meta name="twitter:card" content="summary" />
+      {/* <meta name="twitter:card" content="summary" />
       <meta name="twitter:url" content="https://yourdomain.com" />
-      <meta name="twitter:title" content="PWA App" />
-      <meta name="twitter:description" content="Best PWA App in the world" />
+      <meta name="twitter:title" content="Factory App" />
+      <meta name="twitter:description" content="Factory controle de empréstimos" />
       <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-      <meta name="twitter:creator" content="@DavidWShadow" />
+      <meta name="twitter:creator" content="@DavidWShadow" /> */}
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="PWA App" />
-      <meta property="og:description" content="Best PWA App in the world" />
-      <meta property="og:site_name" content="PWA App" />
-      <meta property="og:url" content="https://yourdomain.com" />
-      <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+      <meta property="og:title" content="Factory App" />
+      <meta property="og:description" content="Factory controle de empréstimos" />
+      <meta property="og:site_name" content="Factory App" />
+      {/* <meta property="og:url" content="https://yourdomain.com" />
+      <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" /> */}
 
       {/* <!-- apple splash screen images -->       */}
       <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
